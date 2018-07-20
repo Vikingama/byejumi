7a81f867e8ea8473d6dcd06467b09e5534d94b2b
VikingX <Pavilion0321@gmail.com>
2018-07-20 17:48:58 +0800

    hahasha

--- gulp/js/main/whee/whee.js
+++ gulp/js/main/whee/whee.js
@@ -4,80 +4,386 @@ Exercitation ipsum ea, id fugiat short ribs pig pork loin jerky corned beef labo
 Lorem sunt eiusmod capicola turkey.
 Sed pork loin fugiat ground round pork ut, biltong id et laborum elit excepteur voluptate ut beef.
 Dolore laboris burgdoggen ipsum tri-tip. T-bone meatloaf id velit venison.`);
-/*
-    gulp 基于管道的思想...测试、检查、合并、压缩、格式化、浏览器自动刷新、部署文件生成...
-    --->通过 src 读取文件产生数据流
-        --->经过一系列 pipe 操作
-            --->通过 dest 方法将数据流写入文件中...
-    全局安装 gulp 是为了执行 gulp 任务，本地安装 gulp 则是为了调用 gulp 插件的功能...
-*/
-// 一一引用，一一加载...
-var gulp = require("gulp"),
-    // 自动刷新浏览器...
-    livereload = require('gulp-livereload'),
-    uglify = require('gulp-uglify');
-/*
-    使用 gulp-load-plugins 模块，可以加载 package.json 文件中所有的 gulp 模块...
-    var gulp = require('gulp'),
-        gulpLoadPlugins = require('gulp-load-plugins'),
-        plugins = gulpLoadPlugins();
-*/
-gulp.task("taskName", function () {
-    /*
-        任务代码/函数，通过 gulp taskName 调用...
-        task 将以最大的并发数执行，gulp 会一次性运行所有的 task 并且不做任何等待；
-        如果你想要创建一个序列化的 task 队列，并以特定的顺序执行，你需要做两件事：
-            给出一个提示，来告知 task 什么时候执行完毕；
-            告知一个 task 依赖另一个 task 的完成...
-        gulp.task("one", function (cb){
-            cb(err);
-        });
-        gulp.task("two", ["one"], function (){
-            // one 完成之后...
-        });
-        gulp.task("default", ["one", "two"]);
-    */
-});
-/*
-    gulp.src(globs[,options]);
-        输出符合所提供的匹配模式或匹配模式数组的文件夹，可以被 pipe 到别的插件中。
-        gulp.src('client/templates/*.jade')
-        .pipe(jade())
-        .pipe(minify())
-        .pipe(
-            gulp.dest('build/minifiedTemplates')
-        );
-    watch 方法用于指定需要监视符合所提供的匹配模式或者匹配模式的数组的文件。
-    一旦这些文件发生变动，就运行指定任务。
-    gulp.watch(glob, function () {
-    // 任务代码/函数
+function identifyDevice () {
+    var device;
+    var pc = "pc";
+    var android = "android";
+    var iPhone = "iPhone";
+    var iPad = "iPad";
+    var mobile = "mobile";
+    var browser = {
+        versions: function () {
+            var u = navigator.userAgent,
+                app = navigator.appVersion;
+            return {
+                trident: u.indexOf('Trident') > -1,
+                presto: u.indexOf('Presto') > -1,
+                webKit: u.indexOf('AppleWebKit') > -1,
+                gecko: u.indexOf('Gecko') > -1 && u.indexOf('KHTML') == -1,
+                mobile: !!u.match(/AppleWebKit.*Mobile.*/),
+                ios: !!u.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/),
+                android: u.indexOf('Android') > -1 || u.indexOf('Linux') > -1,
+                iPhone: u.indexOf('iPhone') > -1,
+                iPad: u.indexOf('iPad') > -1,
+                webApp: u.indexOf('Safari') == -1
+            };
+        }(),
+        language: (navigator.browserLanguage || navigator.language).toLowerCase()
+    }
+}
+
+identifyDevice();
+
+function SubForm () {
+    var e = $(".snow_nm").val();
+    var p = $(".snow_pwd").val();
+    var bChecked = false;
+    var kk = window.location.host;//for crypet
+    if ($("#login_remember").is(':checked'))
+        bChecked = true;
+    if (/^\w+([-+.]\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/.test(e) == false) {
+        $("#login_noti").text("邮箱格式不正确!");
+        $(".snow_nm").focus()
+    } else if (p.length < 6) {
+        $("#login_noti").text("密码长度过短!")
+    } else {
+        $.ajax({
+            type: "post",
+            url: "/common/api/login_new",
+            error: function () {
+                return false
+            },
+            data: {
+                email: e,
+                pwd: encrypt(p),
+                bCheck: bChecked,
+            },
+            datatype: "json",
+            timeout: 5000,
+            beforeSend: function () { },
+            success: function (msg) {
+                //console.log(msg);
+                if (msg != "") {
+                    if (msg.indexOf('{') >= 0) {
+                        var obj = eval('(' + msg + ')');
+                        var bSign = obj[obj.length - 1];
+                        if (typeof (bSign) != "undefined" && bSign) {
+                        }
+                        if (window.localStorage) {
+                            localStorage.removeItem("sc_unm");
+                            localStorage.removeItem("sc_uid");
+                            localStorage.removeItem("sc_uhd");
+                            localStorage.removeItem("sc_uts");
+                            localStorage.setItem("sc_unm", obj["uname"]);
+                            localStorage.setItem("sc_uid", obj["uid"]);
+                            localStorage.setItem("sc_uhd", obj["header"]);
+                            localStorage.setItem("sc_uts", Date.parse(new Date()));
+                        }
+                        window.location.reload();
+                    } else {
+                        $("#login_noti").text("用户名或密码有误!");
+                    }
+                }
+            }
+        })
+    }
+    return false;
+}
+
+function quit () {
+    $.ajax({
+        type: "post",
+        url: "/common/api/logout_new",
+        error: function () {
+            return false
+        },
+        data: {},
+        datatype: "json",
+        timeout: 5000,
+        beforeSend: function () { },
+        success: function (msg) {
+            if (window.localStorage) {
+                localStorage.removeItem("sc_unm");
+                localStorage.removeItem("sc_uid");
+                localStorage.removeItem("sc_uhd");
+                localStorage.removeItem("sc_uts");
+            } else if (navigator.cookieEnabled) {
+                console.log("where is the cookie?I need to use it!");
+            }
+            window.location.reload();
+        }
     })
-*/
-gulp.task("js", ["css"], function () {
-    // js/app.js：指定确切的文件名...
-    // js/*.js：某个目录所有后缀名为js的文件...
-    // js/**/*.js：某个目录及其所有子目录中的所有后缀名为js的文件...
-    // !js/app.js：除了 js/app.js 以外的所有文件...
-    // *.+(js css)：匹配项目根目录下，所有后缀名为 js 或 css 的文件...
-    gulp.src('js/**/*.js')
-        // gulp.src(['js/**/*.js', '!js/**/*.min.js'])
-        .pipe(uglify())
-        .pipe(gulp.dest("dist/js", {
-            // 写入路径的基准目录...(默认当前目录...)
-            cwd: "./",
-            // 可读可写可执行...
-            mode: 0777
-        }))
-        // 自动刷新浏览器...
-        .pipe(livereload());
+}
+
+function view1Animate () {
+    if ($(".choose_intro").is(":visible")) {
+        $(".choose_intro span.item_1").animate({ "left": "0", "top": "0" }, 300);
+        $(".choose_intro span.item_2").animate({ "left": "92%", "top": "90%" }, 300);
+        $(".choose_intro span.item_3").animate({ "left": "0", "top": "90%" }, 300);
+        $(".choose_intro span.item_4").animate({ "left": "92%", "top": "0" }, 300);
+    } else {
+        $(".anim_frame").animate({ "width": "620px" }, 400);
+    }
+}
+
+function view2Animate (num) {
+    var num = arguments[0] || 0;
+    if (num > 6) return false;
+    $(".what_can_do ul > li").eq(num).fadeIn(150, function () {
+        view2Animate(++num);
+    });
+}
+function view3Animate () {
+    $(".box_wrapper").stop().animate({ "margin-top": "0", "opacity": "1" }, 500, function () { $(".box_wrapper").scrollTop(0); });
+}
+function view4Animate () {
+    $("#cs-text").animate({ "height": "130px" }, 300, function () { $(this).animate({ "opacity": "1" }, 800) })
+}
+function recoverView1 () {
+    if ($(".choose_intro").is(":visible")) {
+        $(".choose_intro span.item_1").animate({ "left": "46%", "top": "42%" });
+        $(".choose_intro span.item_2").animate({ "left": "46%", "top": "42%" });
+        $(".choose_intro span.item_3").animate({ "left": "46%", "top": "42%" });
+        $(".choose_intro span.item_4").animate({ "left": "46%", "top": "42%" });
+    } else {
+        $(".anim_frame").animate({ "width": "100%" });
+    }
+}
+function recoverView2 () {
+    $(".what_can_do ul > li").fadeOut(600);
+}
+function recoverView3 () {
+    $(".box_wrapper").stop().animate({ "opacity": "0", "margin-top": "1000px" }, 3000);
+}
+function recoverView4 () {
+    $("#cs-text").animate({ "opacity": "0" });
+}
+
+
+function LoadViews (index, from) {
+    if (scrollLock == false) {
+        return false;
+    } else {
+        scrollLock = false;
+    }
+    if (from != 1)
+        eval("recoverView" + index + "()");
+    var tmpObj = $("#main_wrapper");
+    var lineHeight = tmpObj.find("#section_page_" + index).height();
+    var calcHeight = '-' + (index - 1) * lineHeight;
+    tmpObj.stop().animate({ "margin-top": calcHeight + "px" }, 600, function () {
+        scrollLock = true;
+        eval("view" + index + "Animate()");
+    });
+}
+
+LoadViews(pageFlag, 0);
+
+function planeFly (obj) {
+    var y = obj.offset().top;
+    var x = obj.offset().left;
+    if (scrollLock == false) {
+        return false;
+    } else {
+        scrollLock = false;
+    }
+    if (x < 0) {
+        obj.stop().animate({ "left": "0px", "top": "0px" }, 1000, function () {
+            $("._btn_nav span.item_cancel").toggle();
+            scrollLock = true;
+        });
+
+    } else {
+        obj.stop().animate({ "left": "200px", "top": "-200px" }, 300, function () {
+            $("._btn_nav span.item_cancel").toggle();
+            obj.css({ "left": "-2100px", "top": "1000px" });
+            scrollLock = true;
+        });
+    }
+}
+
+function ToggleViews (direction) {
+    if (scrollLock == false) {
+        return false;
+    } else {
+        scrollLock = false;
+    }
+    eval("recoverView" + pageFlag + "()");
+    var tmpObj = $("#main_wrapper");
+    var lineHeight = tmpObj.find("#section_page_" + pageFlag).height();
+    var speed = 1000;
+    if (direction == "down") {
+        pageFlag--;
+        speed = 600;
+    } else {
+        pageFlag++;
+    }
+    var calcHeight = '-' + (pageFlag - 1) * lineHeight;
+
+    eval("recoverView" + pageFlag + "()");
+    tmpObj.animate({ "margin-top": calcHeight + "px" }, speed, function () {
+        scrollLock = true;
+        setTimeIcons(pageFlag);
+        eval("view" + pageFlag + "Animate()");
+    });
+}
+
+function encrypt (txt) {
+    var code1 = Math.round(Math.random());
+    var code2 = parseInt(Math.random() * 10);
+    var code3 = parseInt(Math.random() * 3);
+    var code4 = parseInt(Math.random() * 2);
+    var tpl = Array('s', 'n', 'o', 'w', 'c', 'o', 'a', 'l', '+', '=');
+    var res = txt;
+    if (code1 > 0) {
+        res = res.replace(/(.{4})/g, '$1' + tpl[code2]);
+        res = tpl[9] + code1 + res + tpl[code4];
+    } else {
+        res = res.replace(/(.{2})/g, '$1' + tpl[code2]);
+        res = tpl[9] + code1 + res + tpl[code4];
+    }
+    res = res.split("").reverse().join("");
+    return res;
+}
+
+$.fn.chkbox = function () {
+    return $(this).each(function (k, v) {
+        var $this = $(v);
+        if ($this.is(':checkbox') && !$this.data('checkbox-replaced')) {
+            $this.data('checkbox-replaced', true);
+            var $l = $('<label for="' + $this.attr('id') + '" class="chkbox"></label>');
+            var $y = $('<span class="yes">checked</span>');
+            var $n = $('<span class="no">unchecked</span>');
+            var $t = $('<span class="toggle"></span>');
+            $l.append($y, $n, $t).insertBefore($this);
+            $this.addClass('replaced');
+            $this.on('change', function () {
+                if ($this.is(':checked')) { $l.addClass('on'); }
+                else { $l.removeClass('on'); }
+                $this.trigger('focus');
+            });
+            $this.on('focus', function () { $l.addClass('focus') });
+            $this.on('blur', function () { $l.removeClass('focus') });
+            if ($this.is(':checked')) { $l.addClass('on'); }
+            else { $l.removeClass('on'); }
+        }
+    });
+};
+$(':checkbox').chkbox();
+
+$(document).ready(function () {
+    $("#nav_loading").hide();
+    $("._btn_nav span.item_cancel").hide();
+    var myDate = new Date();
+    var todayV = myDate.getDate();
+    $("._btn_nav , ._btn_nav span.item_plane").on({
+        mouseenter: function () {
+            if (!$("._btn_nav span.item_cancel").is(':visible'))
+                $("._btn_nav").css("background-position", "0 -50px");
+        },
+        mouseleave: function () {
+            if (!$("._btn_nav span.item_cancel").is(':visible'))
+                $("._btn_nav").css("background-position", "0 0");
+        },
+        click: function () {
+            planeFly($("._btn_nav span.item_plane"));
+            $("#navgation").stop().fadeToggle("slow");
+            if (!$("._btn_nav span.item_cancel").is(':visible'))
+                $("._btn_nav").css("background-position", "0 -100px");
+            else
+                $("._btn_nav").css("background-position", "0 0px");
+            return false;
+        }
+    });
+
+    $("#navgation .top_nav ul li").click(function () {
+        var lk = $(this).find("a").attr("href");
+        if (typeof (lk) != "undefined")
+            window.location.href = lk;
+        return false;
+    })
+
+    $(".ipt_text").focus(function () {
+        var txt = $(this).attr("data-holder");
+        $(this).attr("placeholder", "")
+        $(this).siblings("legend").html("&nbsp;" + txt + "&nbsp;");
+    });
+    $(".ipt_text").blur(function () {
+        $(this).siblings("legend").html("");
+        var val = $(this).val().trim();
+        var data_holder = $(this).attr("data-holder");
+        if (val.length > 0) {
+        } else {
+            $(this).attr("placeholder", data_holder)
+        }
+    });
+
+    $(".time_zoom span.time_node").on({
+        mouseenter: function () {
+            var id = $(this).attr("data-id");
+            if (id == pageFlag) {
+                return false;
+            }
+        },
+        mouseleave: function () {
+            var id = $(this).attr("data-id");
+            if (id == pageFlag) {
+                return false;
+            }
+        },
+        click: function () {
+            var id = $(this).attr("data-id");
+            if (id != pageFlag) {
+                LoadViews(id, 0);
+                pageFlag = id;
+                setTimeIcons(id);
+            }
+            return false;
+        }
+    });
+
+    document.onkeydown = function (event) {
+        if (event.ctrlKey == 1) {
+            return false;
+        } else if (event.keyCode == 38) {
+            if (pageFlag < 2) return false;
+            ToggleViews("down");
+            return false;
+        } else if (event.keyCode == 40) {
+            if (pageFlag > 3) return false;
+            ToggleViews("top");
+            return false;
+        } else if (event.keyCode == 116) {
+            window.location.reload();
+            return false;
+        } else if (event.keyCode == 27) {
+            if ($("#navgation").is(':visible')) {
+                planeFly($("._btn_nav span.item_plane"));
+                $("#navgation").hide();
+                $("._btn_nav").css("background-position", "0 0px");
+            }
+            return false;
+        } else if (event.keyCode == 13) {
+            $("#btn_sub_form").trigger("click");
+        }
+    }
+    document.body.onmousewheel = function (event) {
+        event = event || window.event;
+        mousewheelEvent(event, event.wheelDelta);
+    };
+
+    if (document.attachEvent) {
+        document.attachEvent("onmousewheel", function (e) {
+            mousewheelEvent(e, e.wheelDelta);
+        });
+    }
+    else if (document.addEventListener) {
+        document.addEventListener("DOMMouseScroll", function (e) {
+            mousewheelEvent(e, e.detail * -40);
+        }, false);
+    }
+
+    $(window).resize(function () {
+        LoadViews(pageFlag, 1);
+    });
 });
-/*
-    default 任务由 html\css\js 三个任务组成...
-    task 方法会并发执行三个任务，每个任务异步调用(不是按照先后顺序执行的)...
-*/
-// 默认任务代码/函数，直接 gulp 即可调用
-gulp.task("default", ["html", "css", "js"]);
-// 设置任务依赖可以使个个任务按顺序执行...
-gulp.task("html", function () { });
-// ["html"] ---> html 执行完后才会执行 css 任务
-gulp.task("css", ["html"], function () { });
