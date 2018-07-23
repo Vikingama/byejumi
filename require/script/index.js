ac7c74bc8ea2e374d9702a67b0c60bb2c1f5ddfe
Viking X <vikingx@Vikinclas.local>
2018-07-23 22:36:24 +0800

    hahasha

--- require/script/index.js
+++ require/script/index.js
@@ -1,7 +1,9 @@
 console.log("i am index.js");
-requirejs.config({
-    baseUrl:"",
-    path:{
-        
+require.config({
+    baseUrl: "script",
+    paths: {
+        jquery: "https://cdn.bootcss.com/jquery/3.3.1/jquery.min.js",
+        main: "main/main.js",
+        submix: "submix/submix.js"
     }
 });
