b9693437ce030d276b94adb5889c0b8372151589
VikingX <Pavilion0321@gmail.com>
2018-07-24 13:45:47 +0800

    hahasha

--- require/script/index.js
+++ require/script/index.js
@@ -1,9 +1,11 @@
 console.log("i am index.js");
-requirejs.config({
+require.config({
+    // 所有模块根目录(默认值是加载 require.js 的 HTML 所处的位置)...
     baseUrl: "script",
     paths: {
-        jquery: "https://cdn.bootcss.com/jquery/3.3.1/jquery.min.js",
+        jquery: "https://cdn.bootcss.com/jquery/3.3.1/jquery.min",
         main: "main/main",
         submix: "submix/submix",
+        plugin: "plugin"
     }
 });
